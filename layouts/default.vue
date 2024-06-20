f2a64c8a612e91fea3cabe513f5911251589d956
doo pyo Hong <dp.hong@dongwha.com>
2024-06-20 09:40:40 +0900

    chore: 메뉴 구조 변경 및 구축  준비 관련 내용 수정
    - 사이드 메뉴 구조명 및 구조 변경
    - 홈 및 소개 페이지 링크 기능 추가
    - 홈 페이지 내용 변경

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,20 +12,23 @@
       <v-navigation-drawer theme="dark" permanent>
         <v-list nav>
           <v-list-item
-            prepend-icon="mdi-email"
-            title="Inbox"
-            value="inbox"
+            title="Home"
+            @click="navigateTo('/', { replace: true })"
           ></v-list-item>
           <v-list-item
-            prepend-icon="mdi-account-supervisor-circle"
-            title="Supervisors"
-            value="supervisors"
+            title="About"
+            @click="navigateTo('/about', { replace: true })"
           ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-clock-start"
-            title="Clock-in"
-            value="clockin"
-          ></v-list-item>
+          <v-list-group value="Posts">
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props" title="Posts"></v-list-item>
+            </template>
+            <v-list-item title="Vue.js / Nuxt.js"></v-list-item>
+            <v-list-item title="Java"></v-list-item>
+            <v-list-item title="Linux / Unix"></v-list-item>
+            <v-list-item title="Windows"></v-list-item>
+            <v-list-item title="Tool tips"></v-list-item>
+          </v-list-group>
         </v-list>
       </v-navigation-drawer>
       <v-main style="height: auto">
